ac4087ddc36005bbfd43af67152f000e440bf8b6
Sergey_Bondarenko <sergey7work@gmail.com>
2021-06-19 16:39:10 +0300

    updated

--- first.py
+++ first.py
@@ -2,11 +2,40 @@ import json
 
 class Stadium(object):
 
-    def __init__(self,name, year, capacity, club):
+    def __init__(self,name='', year='', country='', capacity='', club=''):
         self.name = name
         self.year = year
+        self.country = country
         self.capacity = capacity
         self.club = club
 
+    def __str__(self):
+        return f"{self.name}: {self.year}: {self.country}: {self.capacity}: {self.club}"
 
-print(123)
+def FormateData(instance):
+    result = {}
+    for key in instance.__dict__:
+        result[key] = instance.__dict__[key]
+    return result
+
+def FromDictToInstance(dictionary, cls):
+    obj = cls()
+    obj.name = dictionary['name']
+    obj.year = dictionary['year']
+    obj.country = dictionary['country']
+    obj.capacity = dictionary['capacity']
+    obj.club = dictionary['club']
+    return obj
+
+
+stadium = Stadium("Etihad", 2002, "England", 55000, "Manchester City")
+
+# Сериализация
+serialized = json.dumps(FormateData(stadium))
+
+# Десериализация
+desirialized = json.loads(serialized)
+# print(desialized)
+
+print(FromDictToInstance(desirialized, Stadium))
+
